f0fd65284e9df921ef8f3b8d33ed977b18a6285a
Sergey Lukonin <111298990+RuMa476@users.noreply.github.com>
2023-08-04 20:27:44 +0300

    04/08/23

--- educa/educa/urls.py
+++ educa/educa/urls.py
@@ -29,6 +29,7 @@ urlpatterns = [
     path('course/', include('courses.urls')),
     path('', CourseListView.as_view(), name='course_list'),
     path('students/', include('students.urls')),
+    path('accounts/profile/', CourseListView.as_view, name='course_list')
 ]
 
 if settings.DEBUG:
