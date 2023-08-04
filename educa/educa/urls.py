12c3bfbad51b88fc6e081a594a795c10d9c2510f
Sergey Lukonin <111298990+RuMa476@users.noreply.github.com>
2023-08-04 11:41:25 +0300

    04/08/23

--- educa/educa/urls.py
+++ educa/educa/urls.py
@@ -22,9 +22,9 @@ from courses.views import CourseListView
 
 urlpatterns = [
     path('accounts/login/', auth_views.LoginView.as_view(),
-          name='login'),
+         name='login'),
     path('accounts/logout/', auth_views.LogoutView.as_view(),
-          name='logout'),
+         name='logout'),
     path('admin/', admin.site.urls),
     path('course/', include('courses.urls')),
     path('', CourseListView.as_view(), name='course_list'),
